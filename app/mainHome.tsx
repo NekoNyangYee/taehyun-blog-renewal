ab1f0d2ecaa2a43468995cded674b0d01aeb655e
NekoNyangYee <kth08122570@gmail.com>
2025-02-21 00:34:44 +0900

    add: 메인홈 디비 연결 및 ui 수정

--- app/mainHome.tsx
+++ app/mainHome.tsx
@@ -2,11 +2,14 @@
 
 import { addUserToProfileTable } from "@components/lib/loginUtils";
 import { supabase } from "@components/lib/supabaseClient";
+import { usePostStore } from "@components/store/postStore";
 import { EyeIcon } from "lucide-react";
 import Image from "next/image";
 import { useEffect } from "react";
 
 export default function MainHome() {
+    const { posts, fetchPosts } = usePostStore();
+
     useEffect(() => {
         const addUser = async () => {
             const { data, error } = await supabase.auth.getSession();
@@ -29,116 +32,40 @@ export default function MainHome() {
 
         }
 
+        fetchPosts();
         addUser();
     }, []);
 
     return (
         <div className="p-container">
             <h2 className="text-2xl font-bold mb-4">📢 최신 게시물</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-                {/* 게시물 1 */}
-                <div className="bg-white shadow-md rounded-lg p-4 flex gap-4 items-center border border-gray-200">
-                    <Image
-                        src="/nextjs.jpg"
-                        width={64}
-                        height={64}
-                        alt="NextJS 이미지"
-                        className="rounded-lg object-cover"
-                    />
-                    <div className="flex flex-col flex-1">
-                        <span className="bg-blue-100 text-blue-600 text-xs font-semibold px-2 py-1 rounded-md w-fit">
-                            NextJS
-                        </span>
-                        <span className="text-lg font-semibold mt-2">
-                            🚀 Next.js 블로그 프로젝트 구축하기
-                        </span>
-                        <p className="text-sm text-gray-600">
-                            by 태현 · 작성일: 2025년 02월 04일
-                        </p>
-                        <div className="flex gap-1 items-center text-gray-500 text-sm mt-1">
-                            <EyeIcon size={16} />
-                            230 조회
+            <div className="grid grid-cols-1 sm:grid-cols-1 lg:grid-cols-2 gap-6">
+                {posts.length > 0 && posts.map(post => (
+                    <div key={post.id} className="bg-white shadow-md rounded-lg p-4 flex gap-4 items-center border border-gray-200">
+                        <Image
+                            src="/nextjs.jpg"
+                            width={64}
+                            height={64}
+                            alt="NextJS 이미지"
+                            className="rounded-lg object-cover"
+                        />
+                        <div className="flex flex-col flex-1">
+                            <span className="bg-blue-100 text-blue-600 text-xs font-semibold px-2 py-1 rounded-md w-fit">
+                                NextJS
+                            </span>
+                            <span className="text-lg font-semibold mt-2 turncate">
+                                {post.title}
+                            </span>
+                            <p className="text-sm text-gray-600">
+                                by {post.author_name} · 작성일: {post.created_at}
+                            </p>
+                            <div className="flex gap-1 items-center text-gray-500 text-sm mt-1">
+                                <EyeIcon size={16} />
+                                {post.view_count} 조회
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                {/* 게시물 2 */}
-                <div className="bg-white shadow-md rounded-lg p-4 flex gap-4 items-center border border-gray-200">
-                    <Image
-                        src="/typescript.jpg"
-                        width={64}
-                        height={64}
-                        alt="TypeScript 이미지"
-                        className="rounded-lg object-cover"
-                    />
-                    <div className="flex flex-col flex-1">
-                        <span className="bg-purple-100 text-purple-600 text-xs font-semibold px-2 py-1 rounded-md w-fit">
-                            TypeScript
-                        </span>
-                        <span className="text-lg font-semibold mt-2">
-                            ⚡ TypeScript 핵심 문법 정리
-                        </span>
-                        <p className="text-sm text-gray-600">
-                            by 김개발 · 작성일: 2025년 01월 15일
-                        </p>
-                        <div className="flex gap-1 items-center text-gray-500 text-sm mt-1">
-                            <EyeIcon size={16} />
-                            180 조회
-                        </div>
-                    </div>
-                </div>
-
-                {/* 게시물 3 */}
-                <div className="bg-white shadow-md rounded-lg p-4 flex gap-4 items-center border border-gray-200">
-                    <Image
-                        src="/react.jpg"
-                        width={64}
-                        height={64}
-                        alt="React 이미지"
-                        className="rounded-lg object-cover"
-                    />
-                    <div className="flex flex-col flex-1">
-                        <span className="bg-green-100 text-green-600 text-xs font-semibold px-2 py-1 rounded-md w-fit">
-                            React
-                        </span>
-                        <span className="text-lg font-semibold mt-2">
-                            🔥 React 상태 관리 비교 (Redux vs Zustand)
-                        </span>
-                        <p className="text-sm text-gray-600">
-                            by 이리액트 · 작성일: 2025년 01월 10일
-                        </p>
-                        <div className="flex gap-1 items-center text-gray-500 text-sm mt-1">
-                            <EyeIcon size={16} />
-                            310 조회
-                        </div>
-                    </div>
-                </div>
-
-                {/* 게시물 4 */}
-                <div className="bg-white shadow-md rounded-lg p-4 flex gap-4 items-center border border-gray-200">
-                    <Image
-                        src="/tailwind.jpg"
-                        width={64}
-                        height={64}
-                        alt="Tailwind 이미지"
-                        className="rounded-lg object-cover"
-                    />
-                    <div className="flex flex-col flex-1">
-                        <span className="bg-pink-100 text-pink-600 text-xs font-semibold px-2 py-1 rounded-md w-fit">
-                            CSS
-                        </span>
-                        <span className="text-lg font-semibold mt-2">
-                            🎨 Tailwind CSS 활용법 정리
-                        </span>
-                        <p className="text-sm text-gray-600">
-                            by 박UI · 작성일: 2025년 01월 05일
-                        </p>
-                        <div className="flex gap-1 items-center text-gray-500 text-sm mt-1">
-                            <EyeIcon size={16} />
-                            145 조회
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </div>
     );
--- store/postStore.ts
+++ store/postStore.ts
@@ -1,10 +1,12 @@
+import { supabase } from "@components/lib/supabaseClient";
 import { create } from "zustand";
 
-interface PostStore {
+interface PostState {
     id: number;
     title: string;
     contents: string;
     author_id: string;
+    author_name: string;
     status?: string;
     visibility?: string;
     created_at: string;
@@ -12,4 +14,41 @@ interface PostStore {
     view_count?: number;
     like_count?: number;
     category_id: number;
-}
+};
+
+interface PostsProps {
+    posts: PostState[];
+    fetchPosts: () => Promise<void>;
+};
+
+export const usePostStore = create<PostsProps>((set) => ({
+    posts: [],
+    fetchPosts: async () => {
+        const { data, error } = await supabase.from("posts")
+            .select(`id, title, contents, author_id, status, visibility, created_at, updated_at, 
+                     view_count, like_count, category_id, author_name`);
+
+        if (error) {
+            console.log("게시물 불러오는 도중 에러", error);
+        }
+
+        if (data) {
+            set({
+                posts: data.map(post => ({
+                    id: post.id,
+                    title: post.title,
+                    contents: post.contents,
+                    author_id: post.author_id,
+                    author_name: post.author_name,
+                    status: post.status,
+                    visibility: post.visibility,
+                    created_at: post.created_at,
+                    updated_at: post.updated_at,
+                    view_count: post.view_count,
+                    like_count: post.like_count,
+                    category_id: post.category_id
+                })),
+            })
+        }
+    }
+}));
