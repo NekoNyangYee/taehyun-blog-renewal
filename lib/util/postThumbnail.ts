7c61ba3e2a643514064b2c17ea0987c0fee14151
NekoNyangYee <kth08122570@gmail.com>
2025-03-26 19:23:26 +0900

    add: 리눅스 이미지 추가

--- lib/util/postThumbnail.ts
+++ lib/util/postThumbnail.ts
@@ -1,10 +1,11 @@
 const categoryImages: { [key: string]: string } = {
-    "React": "/react.png",
-    "TypeScript": "/typescript.png",
-    "JavaScript": "/javascript.jpg",
-    "NodeJS": "/nodejs.jpg",
-    "미분류": "/default.png",
-    "NextJS": "/nextjs.png",
+  React: "/react.png",
+  TypeScript: "/typescript.png",
+  JavaScript: "/javascript.jpg",
+  NodeJS: "/nodejs.jpg",
+  미분류: "/default.png",
+  NextJS: "/nextjs.png",
+  "System-Security": "/linux.jpg",
 };
 
 export default categoryImages;
